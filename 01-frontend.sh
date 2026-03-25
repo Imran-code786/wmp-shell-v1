af8f7b53f9a5d040d5d1c56a9303382521ef81cb
Imran <imrankhanmohammed130@gmail.com>
2026-03-25 15:37:21 -0500

    learn shllz

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -4,3 +4,26 @@ dnf install -y nginx
 
 systemctl enable nginx
 systemctl start nginx
+
+curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
+dnf install -y nodejs
+
+node --version
+npm --version
+
+curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz
+mkdir -p /tmp/frontend
+cd /tmp/frontend
+tar xzf /tmp/frontend.tar.gz
+
+
+npm ci
+npm run build
+
+rm -rf /usr/share/nginx/html/*
+cp -r /tmp/frontend/dist/* /usr/share/nginx/html/
+
+rm -f /etc/nginx/conf.d/default.conf
+
+nginx -t
+systemctl restart nginx
