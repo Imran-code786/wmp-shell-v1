303e9e0e3e354b34d898c2441ba56c0f509ac831
Imran <imrankhanmohammed130@gmail.com>
2026-03-25 16:04:32 -0500

    learn shllz

--- 02-postgresql.sh
+++ 02-postgresql.sh
@@ -4,3 +4,8 @@ dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_6
 dnf -qy module disable postgresql
 
 dnf install -y postgresql16-server postgresql16
+
+/usr/pgsql-16/bin/postgresql-16-setup initdb
+
+systemctl enable postgresql-16
+systemctl start postgresql-16
