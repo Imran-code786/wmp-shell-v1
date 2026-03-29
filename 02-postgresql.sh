afd8eca2b511819bd47a0fe82cf36ebf1471e2ed
Imran <imrankhanmohammed130@gmail.com>
2026-03-28 23:13:52 -0500

    learn shllz

--- 02-postgresql.sh
+++ 02-postgresql.sh
@@ -1,15 +1,7 @@
 dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-
-
 dnf -qy module disable postgresql
-
 dnf install -y postgresql16-server postgresql16
-
 /usr/pgsql-16/bin/postgresql-16-setup initdb
-
-systemctl enable postgresql-16
-systemctl start postgresql-16
-
 
 sed -i "/listen_addresses/ c listen_addresses = '*'" /var/lib/pgsql/16/data/postgresql.conf
 #sed -i '/replication/! s/peer/trust/' /var/lib/pgsql/16/data/pg_hba.conf
